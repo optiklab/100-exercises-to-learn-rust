df77565b381b3d966eaf8b0e1211a59ebde883f5
Anton Yarkov <antonyarkov+microsoft@microsoft.com>
2025-05-28 21:15:35 +0200

    finished basic calc

--- exercises/02_basic_calculator/09_saturating/src/lib.rs
+++ exercises/02_basic_calculator/09_saturating/src/lib.rs
@@ -3,7 +3,11 @@ pub fn factorial(n: u32) -> u32 {
     for i in 1..=n {
         // Use saturating multiplication to stop at the maximum value of u32
         // rather than overflowing and wrapping around
-        result *= i;
+        if (result * i < result) || (result * i > u32::MAX) {
+            return u32::MAX;
+        } else {
+            result *= i;
+        }
     }
     result
 }
