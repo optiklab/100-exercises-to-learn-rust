3b96ab9d5af7c4cb0a8f7b352654adae10f9f3e8
Anton Yarkov <antonyarkov+microsoft@microsoft.com>
2025-07-27 10:45:29 +0200

    In progress V2

--- exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
+++ exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
@@ -38,7 +38,15 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> &str {
-        todo!()
+        match &self.status {
+            Status::InProgress { assigned_to: person } => person,
+            Status::Done | Status::ToDo => {
+                panic!(
+                    "Only `In-Progress` tickets can be \
+                    assigned to someone"
+                )
+            }
+        }
     }
 }
 
