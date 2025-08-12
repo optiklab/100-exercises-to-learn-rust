b693d9b6dc724b3f582db1f11b73f05c21a22104
Anton Yarkov <antonyarkov+microsoft@microsoft.com>
2025-08-12 13:48:06 +0200

    Use thiserror and implement TryFrom

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -1,11 +1,38 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
 //  The parsing should be case-insensitive.
+use thiserror::Error;
+
+
+#[derive(Error, Debug)]
+enum TicketError {
+    #[error("Ticked status is undefined.")]
+    SomeError
+}
 
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+impl TryFrom<String> for Status {
+    type Error = TicketError; 
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Status::try_from(value.as_str())
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = TicketError; 
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(TicketError::SomeError)
+        }
+    }
 }
 
 #[cfg(test)]
